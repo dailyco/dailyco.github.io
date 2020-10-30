be5b428ae5b60d442c3c78f13ca436ead61ba466
YuJin-Kim <21700140@handong.edu>
2020-10-31 00:20:36 +0900

    Happy ending site 배포하기

---
+++ happy_ending/precache-manifest.541b1a9f87f9d39ccbe2e5e9aa134a4d.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6cbaa7956882cc26552aff8f180a66",
+    "url": "/happy_ending/index.html/index.html"
+  },
+  {
+    "revision": "ea37d73603c263c329c3",
+    "url": "/happy_ending/index.html/static/css/main.90c299a6.chunk.css"
+  },
+  {
+    "revision": "a58fb8a5a3f5a7dba3e8",
+    "url": "/happy_ending/index.html/static/js/2.fd278f41.chunk.js"
+  },
+  {
+    "revision": "c5b77a8dfc51bd7bd701c440e9afe1cb",
+    "url": "/happy_ending/index.html/static/js/2.fd278f41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea37d73603c263c329c3",
+    "url": "/happy_ending/index.html/static/js/main.d5a36a8e.chunk.js"
+  },
+  {
+    "revision": "039fcce2cef7c2348658",
+    "url": "/happy_ending/index.html/static/js/runtime-main.07662449.js"
+  },
+  {
+    "revision": "127b16260b488c3c65cb4f696c3ff65b",
+    "url": "/happy_ending/index.html/static/media/0_Loadingpage.127b1626.mp4"
+  },
+  {
+    "revision": "4ab6474e52406cb9d8f2671d6f86b245",
+    "url": "/happy_ending/index.html/static/media/10-after travel.4ab6474e.mp4"
+  },
+  {
+    "revision": "c00f8d96e3dc85892b6dd984df9fd965",
+    "url": "/happy_ending/index.html/static/media/11-after home.c00f8d96.mp4"
+  },
+  {
+    "revision": "9c38c368cf53437e2fa3aaf59d55849d",
+    "url": "/happy_ending/index.html/static/media/12-happiest moment_home.9c38c368.png"
+  },
+  {
+    "revision": "df40017054588e43313d040467d3289f",
+    "url": "/happy_ending/index.html/static/media/12-happiest moment_travel.df400170.png"
+  },
+  {
+    "revision": "03fabf27e1a67c91140239252df3f505",
+    "url": "/happy_ending/index.html/static/media/13-erase.03fabf27.mp4"
+  },
+  {
+    "revision": "1838cef56c2c0197ab1ea813299e0111",
+    "url": "/happy_ending/index.html/static/media/14-will.1838cef5.png"
+  },
+  {
+    "revision": "148182fa2e7918644c2b53c3a47d0eaa",
+    "url": "/happy_ending/index.html/static/media/15-D-3.148182fa.mp4"
+  },
+  {
+    "revision": "4d51031dc33179cb54671772084c1a90",
+    "url": "/happy_ending/index.html/static/media/16-1-photo frame.4d51031d.png"
+  },
+  {
+    "revision": "2ccbf262b97fadca950a1ba933ef9563",
+    "url": "/happy_ending/index.html/static/media/16-photo booth.2ccbf262.png"
+  },
+  {
+    "revision": "011f2e729e13bf394a2e4518e0a3cc20",
+    "url": "/happy_ending/index.html/static/media/17-BG.011f2e72.png"
+  },
+  {
+    "revision": "21a16e57e73bd5fbba2b58d96b47a87b",
+    "url": "/happy_ending/index.html/static/media/17_front.21a16e57.mp4"
+  },
+  {
+    "revision": "b8b0d11b69cfc1a7d6987b31ff69d2af",
+    "url": "/happy_ending/index.html/static/media/18-D-2.b8b0d11b.mp4"
+  },
+  {
+    "revision": "f7bd4165e3548baed9460285b8a2e39f",
+    "url": "/happy_ending/index.html/static/media/19-music.f7bd4165.mp4"
+  },
+  {
+    "revision": "f12d93b403efb2e718aa1aca87c9ccb0",
+    "url": "/happy_ending/index.html/static/media/1F.f12d93b4.png"
+  },
+  {
+    "revision": "f4aa7004834310dd864c3e3b889096bd",
+    "url": "/happy_ending/index.html/static/media/1SD.f4aa7004.png"
+  },
+  {
+    "revision": "5ff25fb7b448cbc391744c4ed0f76ea0",
+    "url": "/happy_ending/index.html/static/media/2-description.5ff25fb7.png"
+  },
+  {
+    "revision": "4923f4e99836da79fdced22b2905c238",
+    "url": "/happy_ending/index.html/static/media/20-funeral mood.4923f4e9.png"
+  },
+  {
+    "revision": "ef8ea7668bad2251c814af396761585a",
+    "url": "/happy_ending/index.html/static/media/21-most crying person.ef8ea766.png"
+  },
+  {
+    "revision": "9962233b2d327567804f71c94a7c3798",
+    "url": "/happy_ending/index.html/static/media/22-D-1.9962233b.mp4"
+  },
+  {
+    "revision": "bb7fc0279c5c4de373f3697338db8cc8",
+    "url": "/happy_ending/index.html/static/media/23-after D-1.bb7fc027.mp4"
+  },
+  {
+    "revision": "a88b1e8f40172ee124206bc08f0c3093",
+    "url": "/happy_ending/index.html/static/media/24-grave.a88b1e8f.png"
+  },
+  {
+    "revision": "5401a9db403ae64a2fb5e3c54482fb3e",
+    "url": "/happy_ending/index.html/static/media/25-death.5401a9db.mp4"
+  },
+  {
+    "revision": "73d03263daadc5a998658c53443a65ce",
+    "url": "/happy_ending/index.html/static/media/26-last words.73d03263.png"
+  },
+  {
+    "revision": "d73c9682b7920f8731e2d0b96fa7bead",
+    "url": "/happy_ending/index.html/static/media/27-last page.d73c9682.mp4"
+  },
+  {
+    "revision": "67aea676edd0fd051e18d5af13d121e2",
+    "url": "/happy_ending/index.html/static/media/2F.67aea676.png"
+  },
+  {
+    "revision": "e603ad9a856343c855cf12dffdec6aa8",
+    "url": "/happy_ending/index.html/static/media/2SD.e603ad9a.png"
+  },
+  {
+    "revision": "3a85d81ab3074ca516a3eb5b2bd265d4",
+    "url": "/happy_ending/index.html/static/media/3-prologue.3a85d81a.png"
+  },
+  {
+    "revision": "637335cc5ee3cffa90a6d83f6dba4374",
+    "url": "/happy_ending/index.html/static/media/3F.637335cc.png"
+  },
+  {
+    "revision": "8a0a1be9f95dafdf0979eb971085fc0c",
+    "url": "/happy_ending/index.html/static/media/3SD.8a0a1be9.png"
+  },
+  {
+    "revision": "1407ffff340b78f20e6bef58a5d65b90",
+    "url": "/happy_ending/index.html/static/media/4-book.1407ffff.mp4"
+  },
+  {
+    "revision": "fe26a862298556c1ac7b992cef381fb1",
+    "url": "/happy_ending/index.html/static/media/404notfound.fe26a862.png"
+  },
+  {
+    "revision": "95e2ac473b6053084614eb0e8b977cc2",
+    "url": "/happy_ending/index.html/static/media/4F.95e2ac47.png"
+  },
+  {
+    "revision": "472bb8236104dd5e89f84484d7358059",
+    "url": "/happy_ending/index.html/static/media/4SD.472bb823.png"
+  },
+  {
+    "revision": "1690e29c54e5ab4b4ab630da1e7d2b29",
+    "url": "/happy_ending/index.html/static/media/5-calender.1690e29c.mp4"
+  },
+  {
+    "revision": "83595c434e55cb05406c234dc956b3a8",
+    "url": "/happy_ending/index.html/static/media/5F.83595c43.png"
+  },
+  {
+    "revision": "6a129a6c19ea8f35576824bb5a9f6db1",
+    "url": "/happy_ending/index.html/static/media/5SD.6a129a6c.png"
+  },
+  {
+    "revision": "04871d02a09571845ef2a447d2cb2b34",
+    "url": "/happy_ending/index.html/static/media/6-tell or not-tran a.04871d02.mp4"
+  },
+  {
+    "revision": "9f4d450bdc5c2d05258d70a3ef53b7e5",
+    "url": "/happy_ending/index.html/static/media/6-tell or not-tran b.9f4d450b.mp4"
+  },
+  {
+    "revision": "cd305e07bf046e9c5663aad74636f435",
+    "url": "/happy_ending/index.html/static/media/6-tell or not.cd305e07.mp4"
+  },
+  {
+    "revision": "2b71026b3243b2375f1923e32e964df5",
+    "url": "/happy_ending/index.html/static/media/7-1-coins.2b71026b.png"
+  },
+  {
+    "revision": "67b51f75cccab1b411bf544160486f54",
+    "url": "/happy_ending/index.html/static/media/7-2-piggy bank.67b51f75.png"
+  },
+  {
+    "revision": "9d2db5303b2059acb87f324c95167b1b",
+    "url": "/happy_ending/index.html/static/media/7-break or not.9d2db530.mp4"
+  },
+  {
+    "revision": "f7a9da36b002506a04d02140d761ad5b",
+    "url": "/happy_ending/index.html/static/media/7-break.f7a9da36.mp4"
+  },
+  {
+    "revision": "b76f0771cd45e385f321d1413582d82d",
+    "url": "/happy_ending/index.html/static/media/8-1-1-travel.b76f0771.mp4"
+  },
+  {
+    "revision": "9e44761a403124c8ff0bdeb11b9d4bdb",
+    "url": "/happy_ending/index.html/static/media/8-1-1-travel_tran.9e44761a.mp4"
+  },
+  {
+    "revision": "5dd0554ac2d0902173e796c50afbcadf",
+    "url": "/happy_ending/index.html/static/media/8-1-travel alone_together.5dd0554a.mp4"
+  },
+  {
+    "revision": "1d6207db8c304a171d86174c773fe6f4",
+    "url": "/happy_ending/index.html/static/media/8-2-1-stay home alone.1d6207db.mp4"
+  },
+  {
+    "revision": "95b6470b6d7b6e7928aaabf1ef546849",
+    "url": "/happy_ending/index.html/static/media/8-2-2_stay with at home.95b6470b.png"
+  },
+  {
+    "revision": "44549810a3e656719b88c067f596a0d3",
+    "url": "/happy_ending/index.html/static/media/8-2-stay home choose.44549810.png"
+  },
+  {
+    "revision": "5a21095e0a71df9420152c6237be39d2",
+    "url": "/happy_ending/index.html/static/media/8_travel or not.5a21095e.jpg"
+  },
+  {
+    "revision": "99da366c76df2d27adacf80e0fb4ff13",
+    "url": "/happy_ending/index.html/static/media/9 D-4.99da366c.mp4"
+  },
+  {
+    "revision": "13a0b42bd8c3b9760dee00a3377d863a",
+    "url": "/happy_ending/index.html/static/media/Camera icon.13a0b42b.svg"
+  },
+  {
+    "revision": "d455aae6d076f4a107819c8e31560096",
+    "url": "/happy_ending/index.html/static/media/Game BGM.d455aae6.mp3"
+  },
+  {
+    "revision": "4348cdddb3afc2e17ea62fe3a9a1e2f0",
+    "url": "/happy_ending/index.html/static/media/Happy Ending Logo_new.4348cddd.png"
+  },
+  {
+    "revision": "9a85d63e6d2289670ba5d22b2e1dbcfc",
+    "url": "/happy_ending/index.html/static/media/Nature BGM.9a85d63e.mp3"
+  },
+  {
+    "revision": "73fec628638651a69d144f982e964de6",
+    "url": "/happy_ending/index.html/static/media/Subway-Black.73fec628.ttf"
+  },
+  {
+    "revision": "f6b88c40c78a379f8c5d9efdb257f475",
+    "url": "/happy_ending/index.html/static/media/Subway-Shadow.f6b88c40.ttf"
+  },
+  {
+    "revision": "37bcc5c0af4ccbd608ceb7de2b2d5b7c",
+    "url": "/happy_ending/index.html/static/media/first page.37bcc5c0.png"
+  },
+  {
+    "revision": "5740bc029195e1dca713c1e91389e85b",
+    "url": "/happy_ending/index.html/static/media/heartbeat.5740bc02.mp3"
+  },
+  {
+    "revision": "d964edc65e1bae8c11d0687744528d45",
+    "url": "/happy_ending/index.html/static/media/will-del.d964edc6.svg"
+  }
+]);
